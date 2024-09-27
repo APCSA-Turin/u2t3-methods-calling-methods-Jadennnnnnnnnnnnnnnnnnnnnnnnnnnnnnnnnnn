052e58d4ab76a64835e473d3f8f418a9345eb92c
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-09-27 12:50:11 +0000

    commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Letter myLetter = new Letter("The Javanians");
+          // write letters to various people
+        myLetter.writeLetter("Amy");
+        myLetter.writeLetter("Ethan");
+        myLetter.writeLetter("Bonnie");
+        myLetter.writeLetter("Scott");
+    }
+ }
+ 
